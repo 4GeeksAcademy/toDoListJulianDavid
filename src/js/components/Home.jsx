572b2b9489aa2e78a2cbfa5d83353902dad8c8bf
Julián David Pineda Betancurth <89669806+juliandavidpineda@users.noreply.github.com>
2025-07-05 20:03:20 +0000

    Inicio

--- src/js/components/Home.jsx
+++ src/js/components/Home.jsx
@@ -1,26 +1,47 @@
 import React from "react";
 
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
+import { useState } from "react";
 
-//create your first component
+
+
 const Home = () => {
-	return (
-		<div className="text-center">
-            
 
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+	const [tasks, setTasks] = useState([
+		"1",
+		"2",
+		"3",
+		"4"
+	])
+
+	const [createTask, setCreateTask] = useState();
+	
+	let addTask =(key) => {
+		if (key === "Enter"){
+			setTasks([...tasks, createTask.trim()])
+		}
+	}
+
+	    let deleteTask = (index) =>{
+        setTasks(tasks.filter((item, i)=> index != i ))
+    }
+
+
+
+	return (
+		<div className="card text-center mt-5">
+            <div className="card-body">
+				<div>
+					<h1 className="card-title">toDoList</h1>
+					<input onChange={event => setCreateTask(event.target.value)} type="text" value={createTask || " "}
+					onKeyUp={event =>addTask(event.key)}
+					/>
+				</div>
+				
+				{tasks.map((tarea, index) => {
+					return <p key={index}> {tarea} </p>
+					})
+				}
+			</div>
 		</div>
 	);
 };
