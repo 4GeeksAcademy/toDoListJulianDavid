902e9a53a7d78f746e6ca1fb13a3bf637a45df2d
Julián David Pineda Betancurth <89669806+juliandavidpineda@users.noreply.github.com>
2025-07-08 03:24:43 +0000

    crear tareas

--- src/js/components/Home.jsx
+++ src/js/components/Home.jsx
@@ -13,22 +13,49 @@ const Home = () => {
 	const [createTask, setCreateTask] = useState("");
 
 	const onLoad = () => {
-		fetch(apiUrl).then(response =>{
+		fetch(apiUrl).then(response => {
 			return response.json()
 		}).then(datos => {
 			setTasks(datos.todos)
 		})
 	}
 
-	useEffect(onLoad, []) 
+	useEffect(onLoad, [])
+
 
 	let addTask = (tecla) => {
 		if (tecla === "Enter") {
 			if (createTask === "") {
 				return;
 			}
-			setTasks([...tasks, createTask.trim()])
-			setCreateTask("")
+			fetch('https://playground.4geeks.com/todo/todos/julian', {
+				method: "POST",
+				body: JSON.stringify({
+					label: createTask,
+					is_done: false
+				}),
+				headers: {
+					"Content-Type": "application/json"
+				}
+			})
+				.then(resp => {
+					console.log(resp.ok); // Será true si la respuesta es exitosa
+					if (resp.ok ) {
+						// onLoad()
+						setCreateTask("")
+					}
+					console.log(resp.status); // El código de estado 201, 300, 400, etc.
+					return resp.json(); // Intentará parsear el resultado a JSON y retornará una promesa donde puedes usar .then para seguir con la lógica
+				})
+				.then(data => {
+					// Aquí es donde debe comenzar tu código después de que finalice la búsqueda
+					console.log(data); // Esto imprimirá en la consola el objeto exacto recibido del servidor
+					setTasks([...tasks, data])
+				})
+				.catch(error => {
+					// Manejo de errores
+					console.log(error);
+				});
 		}
 	}
 	const deleteTask = (index) => {
